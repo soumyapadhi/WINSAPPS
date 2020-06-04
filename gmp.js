e83279722742558e1158702449a885ab786bf9d2
soumyapadhi <51946404+soumyapadhi@users.noreply.github.com>
2020-06-04 11:27:39 +0530

    Create gmp.js

---
+++ gmp.js
@@ -0,0 +1,92 @@
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import {REGION} from '../components/ui/REGION';
+import DashNew from "./dashboard";
+
+const useStyles = makeStyles(theme => ({
+    formControl: {
+        margin: theme.spacing(2),
+        minWidth: 150,
+    },
+    root:{
+        flexGrow: 1,
+    },
+    paper:{
+        height: 400,
+        padding: theme.spacing(2)
+    },
+}))
+
+export default function Oneview() {
+    const classes = useStyles();
+
+    const [ub, setub] = useState(true);
+
+    const [auto, setauto] = useState(300000);
+
+    const handleChange = e => {
+      setub(e.target.value)
+    };
+
+    let h1tag = REGION.filter(function(region) {
+        return region.ub == ub
+    }
+  );
+
+    const regionOptions = (
+        <Select
+        labelId="region-label"
+        value={ub}
+        onChange={handleChange}
+        >
+            {REGION.map(record => (
+              <MenuItem value={record.ub}>
+              {record.site}
+              </MenuItem>
+            ))}
+        </Select>
+    );
+
+    return (
+            <div className={classes.root}>
+                <Grid container spacing={3}>
+                    <Grid item xs={12}>
+                        <FormControl className={classes.formControl}>
+                            <InputLabel id="region-label">Select Region</InputLabel>
+                            { regionOptions }
+                        </FormControl>
+                        <FormControl className={classes.formControl}>
+                            <InputLabel id="refresh-label">Auto Refresh</InputLabel>
+                            <Select
+                            labelId="refresh-label"
+                            value={auto}
+                            onChange={e => setauto(e.target.value)}
+                            >
+                                <MenuItem value={30000}>30 Seconds</MenuItem>
+                                <MenuItem value={60000}>1 Minute</MenuItem>
+                                <MenuItem value={120000}>2 Minutes</MenuItem>
+                                <MenuItem value={180000}>3 Minutes</MenuItem>
+                                <MenuItem value={240000}>4 Minutes</MenuItem>
+                                <MenuItem value={300000}>5 Minutes</MenuItem>
+                            </Select>
+                        </FormControl>
+                    </Grid>
+                </Grid>
+                <Grid container spacing={3}>
+                    <Grid item xs={12} sm={4}>
+                      <Paper className={classes.paper}>
+                         <DashNew
+                         arr1={h1tag}
+                         />
+                      </Paper>
+                    </Grid>
+                </Grid>
+            </div>
+        );
+    }
